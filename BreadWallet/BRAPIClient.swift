5284c464890fd831a1c4aca302613d69fc4a6b3e
0xbrylee <bryanlee@tosblock.com>
2019-08-23 11:14:12 +0900

    Fix: Wrong server which the API is hosted was fixed

--- BreadWallet/BRAPIClient.swift
+++ BreadWallet/BRAPIClient.swift
@@ -159,7 +159,7 @@ func buildRequestSigningString(_ r: URLRequest) -> String {
     var proto = "https"
     
     // host is the server(s) on which the API is hosted
-    var host = "api.breadwallet.com"
+    var host = "api.wallet.tosblock.com"
     
     // isFetchingAuth is set to true when a request is currently trying to renew authentication (the token)
     // it is useful because fetching auth is not idempotent and not reentrant, so at most one auth attempt
